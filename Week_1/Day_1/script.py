c3ea42465714b174fa3998ca3044b5e3149da139
Sprattistics <97857848+J-Spratt@users.noreply.github.com>
2024-11-23 23:18:10 -0600

    Day 1: Variable Assignment, Output with print

--- Week_1/Day_1/script.py
+++ Week_1/Day_1/script.py
@@ -1,2 +1,18 @@
-def example_function():
-    print("Linting is working perfectly!")
+#Day_1: The first Challenge for today was to write your full name and today's date in separate lines of code.
+
+full_name = ("John Spratt")
+todays_date =("11/23/2024")
+
+print (full_name)
+print(todays_date)
+
+#Day_1: The second challenge is to copy the text below and see if we can it with just one print statement.
+
+statement = ("""I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!
+I am feeling happy :-).
+You can follow my progress at https://github.com/J-Spratt/JCodeChronicles""")
+
+
+print(statement)
